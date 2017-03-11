29f61cb19d4fd92e28fa61964feb62fafb764085
alnepravskaya <alnepravskaya@gmail.com>
2017-03-11 17:01:03 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function sum() {
-  // your solution
+	var n = 0;
+	if(arguments.length==0){
+		return n;
+	}
+	for (var j = 0; j < arguments.length; j++) {
+		n = n + arguments[j];
+	}
+	var fn = function () {
+		if (arguments.length == 0) {
+			return n;
+		}
+		for (var i = 0; i < arguments.length; i++) {
+			n = n + arguments[i];
+		}
+		return sum(n);
+	};
+	return fn;
 }
